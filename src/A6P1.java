075c2f4c8802c182a6cfa4a202d1b60a053eddd8
hongjiaohu <a1229472169@gmail.com>
2016-12-16 00:50:00 -0600

    A6,P1
    Create a program with  methods

---
+++ src/A6P1.java
@@ -0,0 +1,79 @@
+/**
+ * 
+ */
+/**
+
+		 *
+
+		 * Name: Hongjiao Hu
+
+		 * Teacher: Mr. Hardman 
+
+		 * Assignment 6, Program 1
+
+		 * Date Last Modified: 2016/12/15
+
+		 *
+
+ */
+/**
+ * @author h.hu
+ *
+ */
+public class A6P1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		displayName();
+		displayBirthday();
+		displayFact();
+		
+		
+	}
+
+	/**
+	 * display UserName will display the UserName to 
+	 * visitors
+	 * 
+	 * @param There are no parameters.
+	 * @return nothing will be returned.
+	 */
+	
+	public static void displayName(){
+	
+		System.out.println("My name: Hongjiao Hu.");
+	}
+	
+	/**
+	 * display User Birthday will display the User Birthday to
+	 * visitors
+	 * 
+	 * @param There are no parameters.
+	 * @return nothing will be returned.
+	 */
+	
+	public static void displayBirthday(){
+		
+		System.out.println("My birthday: 5/19.");
+		
+	}
+	
+	/**
+	 * display Interesting thing will display the Interesting thing to
+	 * visitors
+	 * 
+	 * @param There are no parameters.
+	 * @return nothing will be returned.
+	 */
+	
+	public static void displayFact(){
+		
+		System.out.println("Interesting thing about me: I bought a big bang last week and it was half price this week");
+		
+	}
+	
+}
